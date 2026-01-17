970eac57d405d21b0449cd0387259fe6d126d3c5
Wuizlz <brisenodan12@gmail.com>
2026-01-17 14:09:38 -0600

    two more added commands test

--- launcher.java
+++ launcher.java
@@ -10,9 +10,9 @@ public class launcher {
 
             while (true) {
                 System.out.println("Please make a choice from the following list.");
-                System.out.println("    0: Quit");
-                System.out.println("    1: Run Notepad");
-                System.out.println("    2: Run TaskManager");
+                System.out.println("    0: Quit");  
+                System.out.println("    1: Run TaskManager");
+                System.out.println("    2: Run Notepad");
                 System.out.println("    4: Run Snipping Tool");
                 System.out.println("    5: Run \"About Windows\"");
                 System.out.println("    6: Run \"System Information\"");
@@ -36,10 +36,26 @@ public class launcher {
                     System.exit(0);
                 }
 
-                if (userInput == 2) { // open task manager
+               
+
+                if (userInput == 1) { // open task manager
                     ProcessBuilder pb = new ProcessBuilder("taskmgr");
                     Process p = pb.start();
-                    System.out.println(p.pid());
+                    System.out.println("Started program 1 with pid = " + p.pid());
+                } 
+
+                if(userInput == 2)
+                {
+                    ProcessBuilder pb = new ProcessBuilder("notepad");
+                    Process p = pb.start();
+                    System.out.println("Start program 1 with pid = " + p.pid());
+                }
+
+                if(userInput == 3)
+                {
+                    ProcessBuilder pb = new ProcessBuilder("charmap"); // ProcessBuilder allows to start a new process
+                    Process p = pb.start(); // starts the process
+                    System.out.println("Start program 1 with pid = " + p.pid());
                 }
 
             }
