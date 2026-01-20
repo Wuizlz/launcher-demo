6ef6b2f8adf9bfe10649710e30b9aee299d0e1bd
Wuizlz <brisenodan12@gmail.com>
2026-01-20 13:04:46 -0600

    seeing if flush made a difference

--- launcher.java
+++ launcher.java
@@ -99,9 +99,9 @@ public class Launcher {
                         Process p = pb.start();
 
                         System.out.println("Started program 7 with pid = " + p.pid());
-                        System.out.flush();
+
                         System.out.println("Launcher waiting on Program 7...");
-                        System.out.flush();
+
                         p.waitFor();
                     } catch (InterruptedException e) {
                         System.out.println("Failed to start" + e.getMessage());
@@ -115,9 +115,9 @@ public class Launcher {
                         pb.inheritIO();
                         Process p = pb.start();
                         System.out.println("Started program 8 with pid = " + p.pid());
-                        System.out.flush();
+
                         System.out.println("Launcher waiting on Program 8...");
-                        System.out.flush();
+
                         p.waitFor();
 
                     } catch (InterruptedException e) {
